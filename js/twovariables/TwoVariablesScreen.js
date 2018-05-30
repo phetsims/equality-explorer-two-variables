87212ad555054348c440b1c7607e8de4649665e8
Chris Malley <cmalley@pixelzoom.com>
2018-05-30 14:58:24 -0600

    TODO
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/twovariables/TwoVariablesScreen.js
+++ js/twovariables/TwoVariablesScreen.js
@@ -25,7 +25,7 @@ define( function( require ) {
     options = _.extend( {
 
       // EqualityExplorerScreen options
-      backgroundColorProperty: new Property( 'rgb( 214, 233, 254 )' )
+      backgroundColorProperty: new Property( 'rgb( 214, 233, 254 )' ) //TODO should this be the same color as Variables screen?
     }, options );
 
     EqualityExplorerScreen.call( this,
