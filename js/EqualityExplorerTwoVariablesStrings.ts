8023b7f3a2f5104a28dbfc159132467fcaa0b179
phet-dev <phetadmin@gmail.com>
2026-03-20 14:01:48 -0600

    grunt update  from daily grunt work

--- js/EqualityExplorerTwoVariablesStrings.ts
+++ js/EqualityExplorerTwoVariablesStrings.ts
@@ -20,4 +20,6 @@ type StringsType = {
 
 const EqualityExplorerTwoVariablesStrings = getStringModule( 'EQUALITY_EXPLORER_TWO_VARIABLES' ) as StringsType;
 
+equalityExplorerTwoVariables.register( 'EqualityExplorerTwoVariablesStrings', EqualityExplorerTwoVariablesStrings );
+
 export default EqualityExplorerTwoVariablesStrings;
